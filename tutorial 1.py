9b952eeffb49360afcb88f9862f56e6e17176a1a
mrussell5599 <50896021+mrussell5599@users.noreply.github.com>
2021-08-09 15:37:53 -0400

    Hello Admin!!

--- tutorial 1.py
+++ tutorial 1.py
@@ -12,7 +12,7 @@ def user(name):
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("home"))  
+    return redirect(url_for("user", name="Admin!"))  
 
 if __name__ == "__main__":
     app.run()
