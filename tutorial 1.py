08d5522b8bc98ea097d5f1b679bc8762d81629d4
mrussell5599 <50896021+mrussell5599@users.noreply.github.com>
2021-08-09 14:28:36 -0400

    Update tutorial 1.py

--- tutorial 1.py
+++ tutorial 1.py
@@ -3,10 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return "Hello! This is the Main Page <h1>HELLO<h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run()
