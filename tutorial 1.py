6ca45bac0159d81af7270a618425de7d4b4096da
mrussell5599 <50896021+mrussell5599@users.noreply.github.com>
2021-08-09 14:54:53 -0400

    Add Admin, import redirect, .url_for

--- tutorial 1.py
+++ tutorial 1.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def home():
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))  
 
 if __name__ == "__main__":
     app.run()
