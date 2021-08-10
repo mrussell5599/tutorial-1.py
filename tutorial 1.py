2a20a005e6ffd9aee1a81f5d4fd9206c5ea2082e
mrussell5599 <50896021+mrussell5599@users.noreply.github.com>
2021-08-10 17:22:50 -0400

    Tim's.Website, new website using base.html

--- tutorial 1.py
+++ tutorial 1.py
@@ -1,4 +1,4 @@
-//Python
+//Python-- tutorial 3.py
 
 from flask import Flask, redirect, url_for, render_template
 
@@ -6,26 +6,32 @@ app = Flask(__name__)
 
 @app.route("/<name>")
 def home(name):
-    return render_template("index.html", content=["tim","joe","bill"])
+    return render_template("index.html", content="Testing")
  
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True) 
     
     
- --html
+ --index.html
+{% extends "base.html" %}
+{% block title %}Home Page{% endblock %}
+{% block contet %}
+<h>Test</h>
+{% endblock %} 
+
+    
+ --base.html
+
 <!doctype html>
 <html>
     <head>
-        <title>Home page</title>
+        <title>{% block title %}{% endblock %}</title>
         </head>
         <body>
-            <h1>Home Page!</h1>
-          {% for x in content %}
-          <p>{{x}}</p>
-        
-            {% endfor %}
+            <h1>Tim's.Website</h1>
+            {% block content %}
+            {% endblock %}
         </body>
     </html>
-
 
